f9edc37cfd86b399f1612591e3ca6fc7b50bfbeb
Joey-Jang <joey.ngn.jang@gmail.com>
2022-06-14 23:28:27 +0900

    연관관계 매핑
    단방향 매핑만으로도 이미 연관관계 매핑은 완료
    이후에 양방향 매핑은 필요할 때 추가(테이블에 영향을 주지 않음)
    연관관계 편의 메소드(한쪽에만 만드는 것이 좋음)
    toString, lombok, JSON 등 양방향 매핑시 무한 루프 주의
    setter 대신 change 등의 다른 이름으로 메소드 생성
    Controller에서 Entity 그대로가 아닌 DTO로 변환하여 반환

---
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -0,0 +1,39 @@
+package jpabook.jpashop;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.Team;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("shop");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setName("member1");
+            member.changeTeam(team);
+            em.persist(member);
+
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
